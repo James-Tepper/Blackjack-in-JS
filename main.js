1817905128bec22040b82b1e6d1c8edc3e682d70
James <64048628+James-Tepper@users.noreply.github.com>
2022-09-08 10:28:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
-class Players{
+class Player{
     constructor(name){
         this.name = name;
         this.bets = 500;
         this.hand = [];
         this.still_in = true;
     }
+}
+
+class Card{
+    constructor(index, suit){
+        this.index = index;
+        this.suit = suit;
+        this.value = cardvalues[index];
+    }
+}
+
+function create_card_deck(){
+    const DECK = [];
+    let cardvalues = {
+        "Ace": 11,
+        "Jack": 10,
+        "King": 10,
+        "Queen": 10,
+        "Two": 2,
+        "Three": 3,
+        "Four": 4,
+        "Five": 5,
+        "Six": 6,
+        "Seven": 7,
+        "Eight": 8,
+        "Nine": 9,
+        "Ten": 10,
+    }
+    
+    for (index in cardvalues){
+        for (suit in [Ace, Spaces, Clubs, Hearts]){
+            DECK.push(Card(index, suit));
+        }
+    }
+return DECK;
 }
 
 
@@ -19,7 +53,7 @@ function get_player_names(){
     let players = []
     for (let i = 1; i <= amount; i++){
         let name = prompt(`Player #${i} is your name?`)
-        players.push(new Players(name))
+        players.push(new Player(name))
     }
 
     return players
